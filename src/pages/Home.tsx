a6dd73e9bc8f1eb0a623d1d1528cb157ec8e507f
Aung Kaung Myat <aungakm667@gmail.com>
2022-09-03 23:15:25 +0630

    designing at -why order food- section  and  -how it's work- section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,6 +13,13 @@ import "swiper/css/pagination";
 import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper";
 import { fakeproduct } from "../component/testApi";
 import { BiShoppingBag } from "react-icons/bi";
+import { TbTruckDelivery } from "react-icons/tb";
+import { BiTime } from "react-icons/bi";
+import { MdOutlineFastfood } from "react-icons/md";
+import { BiDish } from "react-icons/bi";
+import pointer from "../img/point.png";
+import pointer2 from "../img/pointer2.png";
+
 const Home = () => {
   return (
     <>
@@ -81,7 +88,7 @@ const Home = () => {
       <div className="section-feature-product">
         <div className="section-feature-product-boder">
           <div className="section-feature-title">
-            <h4 className="feature-title">FEATURE MENU OF WARTEE</h4>
+            <h4 className="feature-title">FEATURE MENU OF WARTEE?</h4>
           </div>
           <div className="section-product-main">
             <div className="product-list">
@@ -100,6 +107,90 @@ const Home = () => {
                   </div>
                 </div>
               ))}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-promo-main">
+        <div className="section-promo-boder">
+          <div className="section-promo-title">
+            <h4 className="promo-title">WHY ORDER FOOD ON WARTEE</h4>
+          </div>
+          <div className="section-promo-about">
+            <div className="promo-about-boder">
+              <div className="promo-about-one">
+                <TbTruckDelivery className="promo-icon-one" />
+                <p className="promo-title-one">Best Delivery</p>
+                <div className="promo-bar"></div>
+                <p className="promo-text-one">
+                  High-quality delivery service delivered to your home
+                </p>
+              </div>
+              <div className="promo-about-two">
+                <BiTime className="promo-icon-two" />
+                <p className="promo-title-two">Fast like Flash</p>
+                <div className="promo-bar"></div>
+                <p className="promo-text-two">
+                  Everything you order at -will be quickly delivered to you
+                </p>
+              </div>
+              <div className="promo-about-three">
+                <MdOutlineFastfood className="promo-icon-three" />
+                <p className="promo-title-three">Fresh Food</p>
+                <div className="promo-bar"></div>
+                <p className="promo-text-three">
+                  Best ingredients to cock the tasty fresh food for you
+                </p>
+              </div>
+              <div className="promo-about-four">
+                <BiDish className="promo-icon-four" />
+                <p className="promo-title-four">Variety Food</p>
+                <div className="promo-bar"></div>
+                <p className="promo-text-four">
+                  A wide Variety of fast food , desserts and drinks
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-guide-main">
+        <div className="section-guide-boder">
+          <img src={pointer} className="pointer1" alt="" />
+          <img src={pointer2} className="pointer2" alt="" />
+          <div className="section-guide-list">
+            <h4 className="guide-title-main">HOW IT'S WORK</h4>
+
+            <div className="guide-one">
+              <p className="guide-title-one">
+                Step <span style={{ color: "#ffb259" }}>One</span>
+              </p>
+              <p className="guide-title-text">
+                "Search the in the menu that you want to order and click add to
+                cart icon"{" "}
+              </p>
+            </div>
+            <div className="guide-bar"></div>
+            <div className="guide-two">
+              <p className="guide-title-two">
+                Step <span style={{ color: "#589873" }}>Two</span>
+              </p>
+              <p className="guide-title-text">
+                "Add the count number of your order List and write your current
+                location such as name of town and street"
+              </p>
+            </div>
+            <div className="guide-bar"></div>
+            <div className="guide-three">
+              <p className="guide-title-three">
+                Step <span style={{ color: "#D85d44" }}>Three</span>
+              </p>
+              <p className="guide-title-text">
+                "Checkout your menu that's on your order list ,select payment
+                method and print your coupon"
+              </p>
             </div>
           </div>
         </div>
